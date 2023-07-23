e9f1bd9de76d1612d19b11488a5edeb50f2133b6
Kyoko <TinaNguyen290@yahoo.com>
2023-07-22 23:12:16 -0700

    updated side bar

--- src/_nav.js
+++ src/_nav.js
@@ -1,26 +1,29 @@
 import React from 'react'
 import CIcon from '@coreui/icons-react'
 import {
-  cilBell,
-  cilCalculator,
   cilChartPie,
-  cilCursor,
   cilDescription,
-  cilDrop,
-  cilNotes,
-  cilPencil,
-  cilPuzzle,
-  cilSpeedometer,
   cilStar,
-  cil,
+  cibUnity,
+  cibBlender,
+  cilPen,
+  cilBrush,
+  cibEbay,
+  cilMedicalCross,
+  cilHouse,
+  cilCart,
+  cilGroup,
+  cilSchool,
+  cilUser,
 } from '@coreui/icons'
 import { CNavGroup, CNavItem, CNavTitle } from '@coreui/react'
 
 const _nav = [
   {
     component: CNavItem,
-    name: 'About me',
+    name: 'About Me',
     to: '/dashboard',
+    icon: <CIcon icon={cilUser} customClassName="nav-icon" />,
     // icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
     // badge: {
     //   color: 'info',
@@ -32,9 +35,15 @@ const _nav = [
     name: 'Software Developer',
   },
   {
+    component: CNavItem,
+    name: "Bachelors' Degree",
+    to: '/charts',
+    icon: <CIcon icon={cilDescription} customClassName="nav-icon" />,
+  },
+  {
     component: CNavGroup,
     name: 'Game Developer',
-    icon: <CIcon icon={cilStar} customClassName="nav-icon" />,
+    icon: <CIcon icon={cibUnity} customClassName="nav-icon" />,
     items: [
       {
         component: CNavItem,
@@ -67,9 +76,102 @@ const _nav = [
     component: CNavItem,
     name: 'Coding Instructor',
     to: '/charts',
+    icon: <CIcon icon={cilSchool} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavTitle,
+    name: 'Animator',
+  },
+  {
+    component: CNavItem,
+    name: '3D Modeling',
+    to: '/charts',
+    icon: <CIcon icon={cibBlender} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavItem,
+    name: '3D Animations',
+    to: '/charts',
+    icon: <CIcon icon={cilPen} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavItem,
+    name: 'Other Art Works',
+    to: '/charts',
+    icon: <CIcon icon={cilBrush} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavTitle,
+    name: 'Japanese',
+  },
+  {
+    component: CNavItem,
+    name: "Minor's Degree",
+    to: '/charts',
+    icon: <CIcon icon={cilDescription} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavItem,
+    name: 'VR Nihongo',
+    to: '/charts',
+    icon: <CIcon icon={cilGroup} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavItem,
+    name: 'Other Works',
+    to: '/charts',
     icon: <CIcon icon={cilChartPie} customClassName="nav-icon" />,
   },
-
+  {
+    component: CNavTitle,
+    name: 'Other',
+  },
+  {
+    component: CNavItem,
+    name: 'Pharmacy Technician',
+    to: '/charts',
+    icon: <CIcon icon={cilMedicalCross} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavItem,
+    name: 'eBay Store',
+    to: '/charts',
+    icon: <CIcon icon={cibEbay} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavGroup,
+    name: 'Retail',
+    icon: <CIcon icon={cilCart} customClassName="nav-icon" />,
+    items: [
+      {
+        component: CNavItem,
+        name: 'CCSF Bookstore',
+        to: '/login',
+      },
+      {
+        component: CNavItem,
+        name: 'Japan Town Retail',
+        to: '/register',
+      },
+    ],
+  },
+  {
+    component: CNavGroup,
+    name: 'Warehouse',
+    icon: <CIcon icon={cilHouse} customClassName="nav-icon" />,
+    items: [
+      {
+        component: CNavItem,
+        name: 'Truepill Warehouse',
+        to: '/login',
+      },
+      {
+        component: CNavItem,
+        name: 'Amazon Warehouse',
+        to: '/register',
+      },
+    ],
+  },
   {
     component: CNavTitle,
     name: 'Extras',
